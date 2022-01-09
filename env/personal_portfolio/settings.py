215d4f356bc0e87c8e40fe6467bd14bcc68c1bfa
DESKTOP-JQAIFQV\User <carlospereira.ti@gmail.com>
2022-01-09 17:34:19 -0300

    deleted app hello world and created a new app called app_projects

--- env/personal_portfolio/settings.py
+++ env/personal_portfolio/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'hello_world',
+    'app_projects',
+    # hello_world app was deleted
+    # 'hello_world', 
 ]
 
 MIDDLEWARE = [
